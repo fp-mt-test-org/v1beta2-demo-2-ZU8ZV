bf7b649804504621d4cf7e81b17e62f24b757f59
Scaffolder <scaffolder@backstage.io>
2021-07-18 17:23:24 +0000

    Initial commit

---
+++ scripts/build.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+jfrog='jfrog-cli'
+
+echo 
+echo "Installing ${jfrog}..."
+brew install "${jfrog}"
+echo "${jfrog} installation complete."
+echo
+jfrog_config_show_output=$(jfrog c show)
+echo "${jfrog_config_show_output}"
+echo
+
+config_name='artifactory'
+
+if ! [[ ${jfrog_config_show_output} =~ Server\sID\:\s*${config_name} ]]; then
+    echo "Configuring ${jfrog}..."
+    jfrog c add "${config_name}" \
+        --url="${artifactory_base_url}" \
+        --user="${artifactory_username}" \
+        --access-token="${artifactory_password}" \
+        --artifactory-url="${artifactory_base_url}/artifactory" \
+        --interactive=false
+    echo "${jfrog} configured."
+    echo
+fi
+
+echo "Building with ${jfrog}..."
+jfrog rt \
+    gradle clean \
+    artifactoryPublish -b build.gradle \
+    --build-name=v1beta2-demo-2-ZU8ZV \
+    --build-number=1 \
+    -Pversion=1.1.0
+echo "Build complete."
+echo
